dfe40fb868fdf6f5ae3283be4223420a5ab48f2b
Ivar Grimstad <Ivar.Grimstad@cybercom.com>
2018-10-10 13:48:47 +0200

    First version running on OpenLiberty, Thorntail and Payara Micro

---
+++ src/main/java/eu/agilejava/duke/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package eu.agilejava.duke;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Ivar Grimstad (ivar.grimstad@cybercom.com)
+ */
+@ApplicationPath("")
+public class ApplicationConfig extends Application {
+}
---
+++ src/main/java/eu/agilejava/duke/StonesResource.java
@@ -0,0 +1,34 @@
+package eu.agilejava.duke;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.metrics.annotation.Metered;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@Path("stones")
+@RequestScoped
+public class StonesResource {
+
+    @Inject
+    @ConfigProperty(name = "color", defaultValue = "gray")
+    private String color;
+
+
+    @GET
+    @Path("{name}")
+    @Produces(APPLICATION_JSON)
+    public Response getStone(@PathParam("name") String name) {
+
+        Stone stone = new Stone(name, color);
+
+        return Response.ok(stone).build();
+    }
+}
