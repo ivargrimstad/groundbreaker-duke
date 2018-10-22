1034fcbf8648165ab9ca87f94b659ecd14132518
Ivar Grimstad <ivar.grimstad@gmail.com>
2018-10-21 17:58:02 -0700

    Update all to run on port 8080

--- dukes-stone-processor-jar/src/main/java/duke/groundbreaker/StonesResource.java
+++ dukes-stone-processor-jar/src/main/java/duke/groundbreaker/StonesResource.java
@@ -6,7 +6,6 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
@@ -17,16 +16,13 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 public class StonesResource {
 
     @Inject
-    @ConfigProperty(name = "color", defaultValue = "gray")
-    private String color;
-
+    @ConfigProperty(name = "stone", defaultValue = "none")
+    private String infinityStone;
 
     @GET
-    @Path("{name}")
     @Produces(APPLICATION_JSON)
-    public Response getStone(@PathParam("name") String name) {
-
-        Stone stone = new Stone(name, color);
+    public Response getStone() {
+        Stone stone = new Stone(infinityStone);
 
         return Response.ok(stone).build();
     }
