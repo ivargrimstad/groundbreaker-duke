3aba27ea120919d07f63b13285bcdc21f76b23b9
Ivar Grimstad <ivar.grimstad@gmail.com>
2018-10-22 13:47:16 -0700

    Return none for misconfigured stone

--- dukes-stone-processor-jar/src/main/java/duke/groundbreaker/StonesResource.java
+++ dukes-stone-processor-jar/src/main/java/duke/groundbreaker/StonesResource.java
@@ -9,6 +9,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Path("stones")
@@ -26,12 +29,20 @@ public class StonesResource {
     @GET
     @Produces(APPLICATION_JSON)
     public Response getStone() {
-        Stone stone = new Stone(infinityStone);
 
         try {
             Thread.sleep(quality * 1_000);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+
+        final List<String> validStones = Arrays.asList("space", "reality", "power", "soul", "mind", "time");
+
+        Stone stone;
+        if (!validStones.contains(infinityStone)) {
+            stone = new Stone("none");
+        } else {
+            stone = new Stone(infinityStone);
         }
 
         return Response.ok(stone).build();
