283df200a016ca0f6c669764263fdd20beb923b0
Ivar Grimstad <ivar.grimstad@gmail.com>
2018-10-23 13:12:18 -0700

    Add script to push to docker hub

---
+++ push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker push groundbreakerduke/dukes-stone-processor:thorntail
+docker push groundbreakerduke/dukes-stone-processor:liberty
+docker push groundbreakerduke/dukes-stone-processor:payara
+docker push groundbreakerduke/dukes-stone-processor:tomee
+docker push groundbreakerduke/dukes-stone-processor:helidon
+docker push groundbreakerduke/dukes-stone-processor:kumuluz
